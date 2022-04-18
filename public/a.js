6c9174a33379683c0f866644ce9c0503337796c4
janalj <44931863+janalj@users.noreply.github.com>
2022-04-18 04:36:19 +0000

    Fixed fonts for after clicking Continue

--- public/a.js
+++ public/a.js
@@ -1,9 +1,24 @@
 "use strict";
 let continueButton = window.document.getElementById("continueButton");
-continueButton.addEventListener("click",replace);
+continueButton.addEventListener("click",()=>{
+  replace();
+  getInputValue();
+});
 
 
 function replace( ) {
+  
  document.getElementById("BeforeContinue").style.display="none";
-  document.getElementById("AfterContinue").style.display= "block";
+  document.getElementById("AfterContinue").style.display= "inline-block";
+
 }
+
+function getInputValue(){
+            // Selecting the "VideoNickname" element and get its value 
+            var inputVal = document.getElementById("VideoNickname").value;
+            // change value 
+          document.getElementById("EnteredName").innerHTML = "'"+inputVal+"'";            
+          
+          
+
+        }

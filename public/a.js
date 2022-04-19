9c5e4112f7bb8d5c9072f4edc30f9d8c9a1cccfc
janalj <44931863+janalj@users.noreply.github.com>
2022-04-19 22:58:39 +0000

    Add Redirection

--- public/a.js
+++ public/a.js
@@ -1,38 +1,21 @@
 "use strict";
 
-
-
-
 let continueButton = window.document.getElementById("continueButton");
 continueButton.addEventListener("click",()=>{
-
   // sendPostRequest
   let conbinedString = Concate();
   console.log("Printing conbinedString: ", conbinedString);
   sendPostRequest("/VideoData",conbinedString);
-  getVideoName(); // return the name of the Video
 
 });
-
-
-function getVideoName(){
-            // Selecting the "VideoNickname" element and get its value 
-             var inputVal = document.getElementById("VideoNickname").value;
-            // change value 
-           document.getElementById("EnteredName").innerHTML = "'"+inputVal+"'";    
-            
-  // return video name
-            
-        }
 
 function Concate(){
   let u = document.getElementById("UserName").value;
   let t = document.getElementById("TikTokURL").value;
   let v = document.getElementById("VideoNickname").value;
-  let data = [u ,t,v];
+  let data = [u,t,v];
   return data;
 }
-
 
 // posting the data
 async function sendPostRequest(url,data) {
@@ -44,6 +27,7 @@ async function sendPostRequest(url,data) {
     body: data });
   if (response.ok) {
     let data = await response.text();
+   
     window.location = "/acknowlegement.html";
     return data;
   } else {

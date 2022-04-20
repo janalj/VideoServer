73d784f126671ff027f58c70cbc797e90053e316
Mahrukh Zubair <102767578+ZMaaz@users.noreply.github.com>
2022-04-20 23:55:28 +0000

    Final Version

--- public/acknowlegement.js
+++ public/acknowlegement.js
@@ -2,4 +2,5 @@
 
 // store the VideoNickname to ackowlegement html
   
-  document.getElementById('EnteredName').innerHTML = sessionStorage.getItem("VideoNickname");
+ // document.getElementById('EnteredName').innerHTML = sessionStorage.getItem("VideoNickname");
+document.getElementById('EnteredName').innerHTML = "'" + sessionStorage.getItem("VideoNickname")+ "'";
